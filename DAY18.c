6beb7588a9360fbf270ccb2b9c68805daa73b723
shirish983 <shirishk819@gmail.com>
2026-02-19 21:43:12 +0530

    Create DAY18.c

---
+++ DAY18.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, k;
+    int arr[100], temp[100];
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    scanf("%d", &k);
+
+    k = k % n;
+
+    for(int i = 0; i < n; i++)
+        temp[(i + k) % n] = arr[i];
+
+    for(int i = 0; i < n; i++)
+        printf("%d ", temp[i]);
+
+    return 0;
+}
