46f5c7025f0a18fe3865bbbc340105073ee91d21
shirish kadian <shirishk819@gmail.com>
2026-03-10 15:29:32 +0530

    Create DAY37.c

---
+++ DAY37.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 1000
+
+int pq[MAX];
+int size = 0;
+
+void insert(int x) {
+    int i = size - 1;
+
+    while (i >= 0 && pq[i] > x) {
+        pq[i + 1] = pq[i];
+        i--;
+    }
+
+    pq[i + 1] = x;
+    size++;
+}
+
+int deleteMin() {
+    if (size == 0) return -1;
+
+    int val = pq[0];
+
+    for (int i = 1; i < size; i++)
+        pq[i - 1] = pq[i];
+
+    size--;
+
+    return val;
+}
+
+int peek() {
+    if (size == 0) return -1;
+    return pq[0];
+}
+
+int main() {
+    int n;
+    char op[10];
+    int x;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%s", op);
+
+        if (strcmp(op, "insert") == 0) {
+            scanf("%d", &x);
+            insert(x);
+        } 
+        else if (strcmp(op, "delete") == 0) {
+            printf("%d\n", deleteMin());
+        } 
+        else if (strcmp(op, "peek") == 0) {
+            printf("%d\n", peek());
+        }
+    }
+
+    return 0;
+}
