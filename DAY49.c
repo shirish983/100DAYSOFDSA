80375b3f6878bc25b67c100ebf2f91ca5c33c204
shirish kadian <shirishk819@gmail.com>
2026-03-22 17:29:46 +0530

    Create DAY49.c

---
+++ DAY49.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Insert into BST
+struct Node* insert(struct Node* root, int value) {
+    if (root == NULL)
+        return createNode(value);
+
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else
+        root->right = insert(root->right, value);
+
+    return root;
+}
+
+// Search in BST
+int search(struct Node* root, int key) {
+    // Base case
+    if (root == NULL)
+        return 0;
+
+    if (root->data == key)
+        return 1;
+
+    if (key < root->data)
+        return search(root->left, key);
+    else
+        return search(root->right, key);
+}
+
+int main() {
+    int n, x, key;
+    struct Node* root = NULL;
+
+    // Input
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    scanf("%d", &key);
+
+    // Output
+    if (search(root, key))
+        printf("Found");
+    else
+        printf("Not Found");
+
+    return 0;
+}
