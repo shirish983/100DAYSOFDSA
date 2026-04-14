7e663e07abc18c90862dbc871b9875d04dd2246d
shirish kadian <shirishk819@gmail.com>
2026-04-14 08:18:47 +0530

    Create DAY73.c

---
+++ DAY73.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000];
+    int freq[26] = {0};
+
+    scanf("%s", s);
+
+    // Step 1: Count frequencies
+    for (int i = 0; s[i] != '\0'; i++) {
+        freq[s[i] - 'a']++;
+    }
+
+    // Step 2: Find first non-repeating character
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (freq[s[i] - 'a'] == 1) {
+            printf("%c\n", s[i]);
+            return 0;
+        }
+    }
+
+    // If none found
+    printf("$\n");
+
+    return 0;
+}
