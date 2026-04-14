d4a9267faf75e2a45571b0c5f65b7bf89cc45ed8
shirish kadian <shirishk819@gmail.com>
2026-04-14 08:17:53 +0530

    Create DAY72.c

---
+++ DAY72.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000];
+    int freq[26] = {0};
+
+    scanf("%s", s);
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        int index = s[i] - 'a';
+
+        if (freq[index] == 1) {
+            printf("%c\n", s[i]);
+            return 0;
+        }
+
+        freq[index]++;
+    }
+
+    printf("-1\n");
+    return 0;
+}
